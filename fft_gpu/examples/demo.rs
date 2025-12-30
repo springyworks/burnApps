046e538b7b5cae285e64bc056bc46a8ca7691773
springyworks <kostoel@gmail.com>
2025-12-30 21:04:51 +0100

    Add example/demo.rs for fft_gpu, refactor main to lib entrypoint

---
+++ fft_gpu/examples/demo.rs
@@ -0,0 +1,7 @@
+use fft_gpu::run;
+
+fn main() {
+    // This example just calls the main run function, effectively running the app.
+    // You can pass arguments like "--on_cpu" or "--generate-video" to this example.
+    run();
+}
